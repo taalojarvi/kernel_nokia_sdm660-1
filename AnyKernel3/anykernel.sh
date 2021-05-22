ba92b122c9ee1cff8ead8995f25eb703777d7561
Thanuj <thanujbalamurali09@gmail.com>
2021-05-22 13:20:08 +0200

    Import AnyKernel3 ~ From https://www.github.com/thanuj10/AnyKernel3 -> NOKIA_SDM660 Branch

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,37 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Is it all just a fantasy... or an Enigma?
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=PL2
+device.name2=DRG
+device.name3=B2N
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
+
+
